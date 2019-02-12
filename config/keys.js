d0f99949d9e0a22d0bb29f3773a2b3b204766c65
willi <willi-gu@hotmail.de>
2019-02-12 04:17:16 +0100

    Changed db connection string's ip

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-	mongoURI: "mongodb://hsclient:raspberry@192.168.0.105/homesys",
+	mongoURI: "mongodb://hsclient:raspberry@192.168.0.108/homesys",
 	mognoLocalURI: "mongodb://hsclient:raspberry@localhost/homesys"
 }
